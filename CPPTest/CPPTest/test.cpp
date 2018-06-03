f2dd1e1ed963ae3a7bebd083c247759a98cbe397
AHAOAHA <ahaoaha_@outlook.com>
2018-06-03 22:36:58 +0800

    Test

---
+++ CPPTest/CPPTest/test.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdlib.h>
+#define _cplusplus 1
+#ifndef _cplusplus
+extern "C" {
+#endif
+	void Test(int a)
+	{}
+	void Test(char b)
+	{}
+#ifndef _cplusplus
+}
+#endif
+int main()
+{
+	system("pause");
+	return 0;
+}
