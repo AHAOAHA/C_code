22afdc327e5254677d3912e42228461d6c97fbb0
AHAOAHA <ahaoaha_@outlook.com>
2018-08-24 20:28:34 +0800

    2048

--- 2048/2048/2048.c
+++ 2048/2048/2048.c
@@ -92,14 +92,123 @@ void MoveMap(int array[ROW][COL])
 	}
 }
 
+struct POS
+{
+	int row;
+	int col;
+};
+
+
 void MovePointW(int array[ROW][COL])
-{}
+{
+	struct POS pos;
+	struct POS start;
+
+	//列遍历二维数组 从上向下
+	for (start.col = 0; start.col < COL; ++start.col)
+	{
+		pos.col = start.col;
+		pos.row = 0;
+		for (start.row = 0; start.row < ROW; ++start.row)
+		{
+			//找出最近的0的位置
+			while (0 != array[pos.row][pos.col] && pos.row < ROW)
+			{
+				++pos.row;
+			}
+			if (pos.row == ROW)
+				continue;
+
+			//在pos下面 如果有位置不是0 就把它换到pos的位置
+			if (0 != array[start.row][start.col] && start.row>pos.row)
+			{
+				array[pos.row][pos.col] = array[start.row][start.col];
+				array[start.row][start.col] = 0;
+			}
+		}
+	}
+}
 void MovePointS(int array[ROW][COL])
-{}
+{
+	struct POS pos;
+	struct POS start;
+
+	//列遍历二维数组 从下向上
+	for (start.col = COL - 1; start.col >= 0; --start.col)
+	{
+		pos.col = start.col;
+		pos.row = ROW - 1;
+		for (start.row = ROW - 1; start.row >= 0; --start.row)
+		{
+			while (0 != array[pos.row][pos.col] && pos.row >= 0)
+			{
+				--pos.row;
+			}
+			if (pos.row == -1)
+				continue;
+			if (0 != array[start.row][start.col] && start.row<pos.row)
+			{
+				array[pos.row][pos.col] = array[start.row][start.col];
+				array[start.row][start.col] = 0;
+			}
+		}
+	}
+}
 void MovePointA(int array[ROW][COL])
-{}
+{
+	struct POS pos;
+	struct POS start;
+
+	//行遍历二维数组 从左向右
+	for (start.row = 0; start.row < ROW; ++start.row)
+	{
+		pos.row = start.row;
+		pos.col = 0;
+		for (start.col = 0; start.col < COL; ++start.col)
+		{
+			//找出最近的0的位置
+			while (0 != array[pos.row][pos.col] && pos.row < COL)
+			{
+				++pos.col;
+			}
+			if (pos.row == COL)
+				continue;
+
+			//在pos后面 如果有位置不是0 就把它换到pos的位置
+			if (0 != array[start.row][start.col] && start.col>pos.col)
+			{
+				array[pos.row][pos.col] = array[start.row][start.col];
+				array[start.row][start.col] = 0;
+			}
+		}
+	}
+}
 void MovePointD(int array[ROW][COL])
-{}
+{
+	struct POS pos;
+	struct POS start;
+
+	//行遍历二维数组 从右向左
+	for (start.row = ROW - 1; start.row >= 0; --start.row)
+	{
+		pos.row = start.row;
+		pos.col = COL - 1;
+		for (start.col = COL - 1; start.col >= 0; --start.col)
+		{
+			while (0 != array[pos.row][pos.col] && pos.col >= 0)
+			{
+				--pos.col;
+			}
+			if (pos.col == -1)
+				continue;
+			if (0 != array[start.row][start.col] && start.col<pos.col)
+			{
+				array[pos.row][pos.col] = array[start.row][start.col];
+				array[start.row][start.col] = 0;
+			}
+		}
+	}
+}
 //////////////////////////游戏主体函数
 int Game(int array[ROW][COL])
 {
