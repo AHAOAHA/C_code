a8bca8dbd7ebaf411f91eeb35f5213c75c996e58
AHAOAHA <ahaoaha_@outlook.com>
2021-05-06 16:56:24 +0800

    Go调用C传入传出函数

--- golang/CGo/main.go
+++ golang/CGo/main.go
@@ -1,11 +1,38 @@
 package main
 
-//#include <stdio.h>
+/*
+struct A {
+    int x;
+    char arc[10];
+    int ari[10];
+};
+void f(struct A* stu) {
+    stu->x = 10;
+    int i = 0;
+    for (i = 0; i < 10; i++) {
+        stu->arc[i] = 'x';
+    }
+    for (i = 0; i < 10; i++) {
+        stu->ari[i] = 9;
+    }
+}
+*/
+import "C"
 
 import (
 	"fmt"
+	"unsafe"
 )
 
+type A struct {
+	x    int32
+	arrc [10]byte
+	arri [10]int32
+}
+
 func main() {
-	fmt.Println("hello world!")
+	var x A
+	ss := (*C.struct_A)(unsafe.Pointer(&x))
+	C.f(ss)
+	fmt.Printf("%+v\n", x)
 }
